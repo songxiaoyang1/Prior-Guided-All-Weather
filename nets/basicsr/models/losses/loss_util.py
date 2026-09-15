3aa1875395161403b6dd199b7c01649459e5fe3c
UltralyticsAssistant <web@ultralytics.com>
2026-09-15 06:43:37 +0000

    Auto-format by https://ultralytics.com/actions

--- nets/basicsr/models/losses/loss_util.py
+++ nets/basicsr/models/losses/loss_util.py
@@ -5,6 +5,7 @@
 # Copyright 2018-2020 BasicSR Authors
 # ------------------------------------------------------------------------
 import functools
+
 from torch.nn import functional as F
 
 
@@ -28,7 +29,7 @@ def reduce_loss(loss, reduction):
         return loss.sum()
 
 
-def weight_reduce_loss(loss, weight=None, reduction='mean'):
+def weight_reduce_loss(loss, weight=None, reduction="mean"):
     """Apply element-wise weight and reduce loss.
 
     Args:
@@ -47,10 +48,10 @@ def weight_reduce_loss(loss, weight=None, reduction='mean'):
         loss = loss * weight
 
     # if weight is not specified or reduction is sum, just reduce the loss
-    if weight is None or reduction == 'sum':
+    if weight is None or reduction == "sum":
         loss = reduce_loss(loss, reduction)
     # if reduction is mean, then compute mean over weight region
-    elif reduction == 'mean':
+    elif reduction == "mean":
         if weight.size(1) > 1:
             weight = weight.sum()
         else:
@@ -63,36 +64,23 @@ def weight_reduce_loss(loss, weight=None, reduction='mean'):
 def weighted_loss(loss_func):
     """Create a weighted version of a given loss function.
 
-    To use this decorator, the loss function must have the signature like
-    `loss_func(pred, target, **kwargs)`. The function only needs to compute
-    element-wise loss without any reduction. This decorator will add weight
-    and reduction arguments to the function. The decorated function will have
-    the signature like `loss_func(pred, target, weight=None, reduction='mean',
-    **kwargs)`.
+    To use this decorator, the loss function must have the signature like `loss_func(pred, target, **kwargs)`. The
+    function only needs to compute element-wise loss without any reduction. This decorator will add weight and reduction
+    arguments to the function. The decorated function will have the signature like `loss_func(pred, target, weight=None,
+    reduction='mean', **kwargs)`.
 
     :Example:
 
-    >>> import torch
-    >>> @weighted_loss
-    >>> def l1_loss(pred, target):
-    >>>     return (pred - target).abs()
+    >>> import torch >>> @weighted_loss >>> def l1_loss(pred, target): >>> return (pred - target).abs()
 
-    >>> pred = torch.Tensor([0, 2, 3])
-    >>> target = torch.Tensor([1, 1, 1])
-    >>> weight = torch.Tensor([1, 0, 1])
+    >>> pred = torch.Tensor([0, 2, 3]) >>> target = torch.Tensor([1, 1, 1]) >>> weight = torch.Tensor([1, 0, 1])
 
-    >>> l1_loss(pred, target)
-    tensor(1.3333)
-    >>> l1_loss(pred, target, weight)
-    tensor(1.5000)
-    >>> l1_loss(pred, target, reduction='none')
-    tensor([1., 1., 2.])
-    >>> l1_loss(pred, target, weight, reduction='sum')
-    tensor(3.)
+    >>> l1_loss(pred, target) tensor(1.3333) >>> l1_loss(pred, target, weight) tensor(1.5000) >>> l1_loss(pred, target,
+    reduction="none") tensor([1., 1., 2.]) >>> l1_loss(pred, target, weight, reduction="sum") tensor(3.)
     """
 
     @functools.wraps(loss_func)
-    def wrapper(pred, target, weight=None, reduction='mean', **kwargs):
+    def wrapper(pred, target, weight=None, reduction="mean", **kwargs):
         # get element-wise loss
         loss = loss_func(pred, target, **kwargs)
         loss = weight_reduce_loss(loss, weight, reduction)
